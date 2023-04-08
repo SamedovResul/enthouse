ea8a7071ebd01d7fd64a9fbe0fb0de6d3563e913
SamedovRasul <samedovrasul7@gmail.com>
2023-04-08 15:17:30 +0400

    add motor ON on get nethod

--- controller/temprature.js
+++ controller/temprature.js
@@ -18,18 +18,18 @@ export const temperatureGet = async (req, res) => {
 
     // console.log(`${minutes} minute(s) and ${seconds} second(s) have`);
 
-    if(minutes <= engine.time - 1){
-      engine = {
-        Motor_OK:1,
-        time:engine.createdAt,
-        timer:engine.time
-      }
-    }else{
-      engine = {
-        Motor_OK:0,
-        time:0,
-      }
-    }
+    // if(minutes <= engine.time - 1){
+    //   engine = {
+    //     Motor_OK:1,
+    //     time:engine.createdAt,
+    //     timer:engine.time
+    //   }
+    // }else{
+    //   engine = {
+    //     Motor_OK:0,
+    //     time:0,
+    //   }
+    // }
 
     res.status(200).json({
       water,
